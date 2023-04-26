d5623845014c11b734f0b7eb111c4ccac44cc2eb
Vgenia14 <vgenia14@gmail.com>
2023-04-26 15:43:54 +0200

    commit5

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -25,7 +25,7 @@ export default function Contacts() {
         process.env.REACT_APP_EMAILJS_SERVICE_ID,
         process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
         e.target,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        "oSg39nE41wY3H5y75"
       )
       .then(
         (result) => {
